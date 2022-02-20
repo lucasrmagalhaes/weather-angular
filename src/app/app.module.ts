0b51616bdccbce9bd8ffa310b9902d3a6ac16094
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-02-20 13:33:52 -0300

    Home: Pesquisa - Carregando no console

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeModule } from './pages/home/home.module';
+import { BookmarksModule } from './pages/bookmarks/bookmarks.module';
 
 @NgModule({
   declarations: [
@@ -10,9 +11,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule,
+    BookmarksModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'jv-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss']
 })
+
 export class HomePage implements OnInit {
+  searchControl: FormControl
 
   constructor() { }
 
   ngOnInit(): void {
+    this.searchControl = new FormControl('', Validators.required);
   }
 
+  doSearch() {
+    console.log(this.searchControl.value)
+  }
 }
